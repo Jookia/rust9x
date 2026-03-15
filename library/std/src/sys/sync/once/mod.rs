rust9x/rust

Implement thread parking, RwLock, Once fallbacks
- Bring back "Generic" thread parker
- Add checks for Win8 and WinXP APIs to decide which thread parker impl to use
- Use `queue::RwLock` for rust9x. Now that the thread parker is available, we get this high-quality `RwLock` implementation on all API levels, without any extra fallback impls!
- Same for `Once`

library/std/src/sys/sync/once/mod.rs
@@ -9,7 +9,7 @@
 
 cfg_if::cfg_if! {
     if #[cfg(any(
-        all(target_os = "windows", not(target_vendor="win7")),
+        all(target_os = "windows", not(any(target_vendor = "win7", target_vendor = "rust9x"))),
         target_os = "linux",
         target_os = "android",
         all(target_arch = "wasm32", target_feature = "atomics"),

library/std/src/sys/sync/rwlock/mod.rs
@@ -1,6 +1,6 @@
 cfg_if::cfg_if! {
     if #[cfg(any(
-        all(target_os = "windows", not(target_vendor = "win7")),
+        all(target_os = "windows", not(any(target_vendor = "win7", target_vendor = "rust9x"))),
         target_os = "linux",
         target_os = "android",
         target_os = "freebsd",
@@ -14,7 +14,7 @@ cfg_if::cfg_if! {
         pub use futex::RwLock;
     } else if #[cfg(any(
         target_family = "unix",
-        all(target_os = "windows", target_vendor = "win7"),
+        all(target_os = "windows", any(target_vendor = "win7", target_vendor = "rust9x")),
         all(target_vendor = "fortanix", target_env = "sgx"),
         target_os = "xous",
     ))] {

Jookia/rust9x

library/std/src/sys/sync/once/mod.rs
@@ -9,7 +9,7 @@
 
 cfg_select! {
     any(
-        all(target_os = "windows", not(target_vendor="win7")),
+        all(target_os = "windows", not(any(target_vendor = "win7", target_family = "rust9x"))),
         target_os = "linux",
         target_os = "android",
         all(target_arch = "wasm32", target_feature = "atomics"),

library/std/src/sys/sync/rwlock/mod.rs
@@ -1,6 +1,6 @@
 cfg_select! {
     any(
-        all(target_os = "windows", not(target_vendor = "win7")),
+        all(target_os = "windows", not(any(target_vendor = "win7", target_family = "rust9x"))),
         target_os = "linux",
         target_os = "android",
         target_os = "freebsd",
@@ -16,7 +16,7 @@ cfg_select! {
     }
     any(
         target_family = "unix",
-        all(target_os = "windows", target_vendor = "win7"),
+        all(target_os = "windows", any(target_vendor = "win7", target_family = "rust9x")),
         all(target_vendor = "fortanix", target_env = "sgx"),
         target_os = "xous",
         target_os = "teeos",