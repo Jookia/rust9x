rust9x/rust

Add downlevel implementations for Mutex and Condvar

library/std/src/sys/sync/condvar/windows7.rs
@@ -12,6 +12,10 @@ unsafe impl Sync for Condvar {}
 
 impl Condvar {
     #[inline]
+    #[cfg_attr(
+        target_vendor = "rust9x",
+        allow(dead_code, reason = "initialized via rust9x::Mutex::new")
+    )]
     pub const fn new() -> Condvar {
         Condvar { inner: UnsafeCell::new(c::CONDITION_VARIABLE_INIT) }
     }

library/std/src/sys/sync/mutex/windows7.rs
@@ -31,6 +31,10 @@ pub unsafe fn raw(m: &Mutex) -> *mut c::SRWLOCK {
 
 impl Mutex {
     #[inline]
+    #[cfg_attr(
+        target_vendor = "rust9x",
+        allow(dead_code, reason = "initialized via rust9x::Mutex::new")
+    )]
     pub const fn new() -> Mutex {
         Mutex { srwlock: UnsafeCell::new(c::SRWLOCK_INIT) }
     }

Jookia/rust9x

library/std/src/sys/sync/condvar/windows7.rs
@@ -12,6 +12,10 @@ unsafe impl Sync for Condvar {}
 
 impl Condvar {
     #[inline]
+    #[cfg_attr(
+        target_family = "rust9x",
+        allow(dead_code, reason = "initialized via rust9x::Mutex::new")
+    )]
     pub const fn new() -> Condvar {
         Condvar { inner: UnsafeCell::new(c::CONDITION_VARIABLE_INIT) }
     }

library/std/src/sys/sync/mutex/windows7.rs
@@ -31,6 +31,10 @@ pub unsafe fn raw(m: &Mutex) -> *mut c::SRWLOCK {
 
 impl Mutex {
     #[inline]
+    #[cfg_attr(
+        target_family = "rust9x",
+        allow(dead_code, reason = "initialized via rust9x::Mutex::new")
+    )]
     pub const fn new() -> Mutex {
         Mutex { srwlock: UnsafeCell::new(c::SRWLOCK_INIT) }
     }